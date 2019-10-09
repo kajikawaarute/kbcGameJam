18fea1545a6c6ef5605a9b8d48493ac609ee527b
Yuto Taketi <kbc18b15@stu.kawahara.ac.jp>
2019-10-09 14:57:25 +0900

    プレイヤーキャラの変更、アニメーション追加　紙ごみのモデルの変更

--- GameTemplate/Game/Player.h
+++ GameTemplate/Game/Player.h
@@ -9,17 +9,25 @@ public:
 	void Update();
 	void Move();
 	void Turn();
-	
+	void AnimationController();
+
 	CVector3 GetPos()
 	{
 		return m_position;
 	}
 
+	enum EnAnimationClip {
+		enAnimationClip_Player_Idle,
+		enAnimationClip_Player_Run,
+		enAnimationClip_num
+	};
 	prefab::CSkinModelRender* m_skinModelRender = nullptr;		//スキンモデルレンダラー。
 	CVector3 m_position = CVector3::Zero;						//座標
 	CVector3 m_moveSpeed = CVector3::Zero;						//移動速度
 	CQuaternion m_rotation = CQuaternion::Identity;				//回転
 	CCharacterController m_charaCon;							//キャラクターコントローラー
+
+	CAnimationClip m_animationClip[enAnimationClip_num];	//アニメーションクリップ。
 	
 };
 
