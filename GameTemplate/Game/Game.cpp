17a6036205ff94c315b94af1b5a2e31bd0209b22
Yuto Taketi <kbc18b15@stu.kawahara.ac.jp>
2019-10-11 11:51:08 +0900

    紙ごみの生成の追加

--- GameTemplate/Game/Game.cpp
+++ GameTemplate/Game/Game.cpp
@@ -29,7 +29,7 @@ Game::Game()
 	m_dirLight->SetColor({ 1000.0f, 1000.0f, 1000.0f, 1.0f });
 	GraphicsEngine().GetShadowMap().SetLightDirection({ 1.0f, 0.0f, 1.0f });
 
-	m_dirLight2->SetDirection({ 0.707f, 0.707f, -0.707f });
+	m_dirLight2->SetDirection({ 1.077f, 0.707f, -0.707f });
 	m_dirLight2->SetColor({ 1000.0f, 1000.0f, 1000.0f, 1.0f });
 	GraphicsEngine().GetShadowMap().SetLightDirection({ 0.0f, 1.0f, 0.0f });
 
@@ -97,6 +97,7 @@ Game::~Game()
 	DeleteGOs("GarbageBox");
 	DeleteGOs("Botol");
 	DeleteGOs("Paper");
+	DeleteGOs("Paper2");
 	DeleteGOs("Generator");
 }
 bool Game::Start()
